43b10254bc435e03170f712387cd241f55f4938a
Krupyansky <krupyansky@gmail.com>
2020-10-18 19:23:09 +0300

    Added section Decoration

--- js/script.js
+++ js/script.js
@@ -20,4 +20,20 @@ $(document).ready(function(){
           .addClass('product__type_active').siblings().removeClass('product__type_active')
           .closest('div.container').find('div.product__types-second').removeClass('product__types-second_active').eq($(this).index()).addClass('product__types-second_active');
     });
+
+    $('div.decoration__tabs').on('click', 'div:not(.decoration__tab_active)', function() {
+        $(this)
+          .addClass('decoration__tab_active').siblings().removeClass('decoration__tab_active')
+          .closest('div.container').find('div.decoration__type').removeClass('decoration__type_active').eq($(this).index()).addClass('decoration__type_active');
+    });
+
+    $('div.decoration__type-content-first').on('click', 'img:not(.decoration__type-content-first-img_active)', function() {
+        $('img.decoration__type-content-first-img_active').removeClass('decoration__type-content-first-img_active');
+        $(this).addClass('decoration__type-content-first-img_active');
+    });
+
+    $('div.decoration__type-content-colors').on('click', 'span:not(.decoration__type-content-color_active)', function() {
+        $('span.decoration__type-content-color_active').removeClass('decoration__type-content-color_active');
+        $(this).addClass('decoration__type-content-color_active');
+    });
 });
